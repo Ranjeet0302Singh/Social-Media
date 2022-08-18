d1935f6b89c1af81d8877f061f9ddc656a9203dd
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-08-18 17:42:02 +0530

    commit

---
+++ JS/script.js
@@ -0,0 +1,144 @@
+// Sidebar
+const menuItems = document.querySelectorAll(".menu-item");
+// Messages
+const messagesNotification = document.querySelector("#messages-notifications");
+const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
+
+// SideBar
+// remove active class form other menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notification-popup").style.display = "none";
+    } else {
+      document.querySelector(".notification-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+
+// Messages
+// search chats
+const searchMessage = () => {
+  const val = messageSearch.value.toLowerCase();
+  message.forEach((chat) => {
+    let name = chat.querySelectorAll("h5").textContent.toLowerCase();
+    if (name.indexOf(val) != -1) {
+      chat.style.display = "flex";
+    } else {
+      chat.style.display = "none";
+    }
+  });
+};
+messageSearch.addEventListener("keyup", searchMessage);
+// heightlight message card when messages menu item is clicked
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
+
+// Theme
+const theme = document.querySelector("#theme");
+const themeModel = document.querySelector(".customize-theme");
+
+const openThemeModel = () => {
+  themeModel.style.display = "grid";
+};
+
+const closeThemeModel = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+    themeModel.style.display = "none";
+  }
+};
+theme.addEventListener("click", openThemeModel);
+
+themeModel.addEventListener("click", closeThemeModel);
+
+// Font Size
+
+const removeSizeSeclector = () => {
+    fontsizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+const fontsizes = document.querySelectorAll(".choose-size span");
+var root =document.querySelector(':root');
+
+fontsizes.forEach((size) => {
+
+
+  size.addEventListener("click", () => {
+    removeSizeSeclector();
+    let fontSize;
+  size.classList.toggle('active');
+    if (size.classList.contains("font-size-1")) {
+      fontSize = "10px";
+      root.style.setProperty('----stickt-top-left', '5.4rem');
+      root.style.setProperty('----stickt-top-right', '5.4rem');
+    } else if (size.classList.contains("font-size-2")) {
+      fontSize = "13px";
+      root.style.setProperty('----stickt-top-left', '5.4rem');
+      root.style.setProperty('----stickt-top-right', '-7rem');
+    } else if (size.classList.contains("font-size-3")) {
+      fontSize = "16px";
+      root.style.setProperty('----stickt-top-left', '-2rem');
+      root.style.setProperty('----stickt-top-right', '-17rem');
+    } else if (size.classList.contains("font-size-4")) {
+      fontSize = "19px";      
+      root.style.setProperty('----stickt-top-left', '-5rem');
+      root.style.setProperty('----stickt-top-right', '-25rem');
+    } else if (size.classList.contains("font-size-5")) {
+      fontSize = "22px";
+      root.style.setProperty('----stickt-top-left', '-12rem');
+      root.style.setProperty('----stickt-top-right', '-35rem');
+    }
+
+    document.querySelector("html").style.fontSize = fontSize;
+  });
+});
+// Change color 
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+colorPalette.forEach(color => {
+    color.addEventListener('click' , () =>{
+        let primaryHue;
+        changeActiveColorClass();
+        if(color.classList.contains('color-1')){
+            primaryHue = 252; 
+        }
+        else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }
+        else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }
+        else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }
+        else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue',primaryHue)
+    })
+})
