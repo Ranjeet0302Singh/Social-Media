ebe87e6dfc897e4445776dbc2dc93af4ad8959a7
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-08-18 19:00:42 +0530

    almost done

--- JS/script.js
+++ JS/script.js
@@ -142,3 +142,52 @@ colorPalette.forEach(color => {
         root.style.setProperty('--primary-color-hue',primaryHue)
     })
 })
+
+// background
+
+const Bg1=document.querySelector('.bg-1');
+const Bg2=document.querySelector('.bg-2');
+const Bg3=document.querySelector('.bg-3');
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+const changeBG = () =>{
+    root.style.setProperty('--light-color-lightness',lightColorLightness);
+    root.style.setProperty('--white-color-lightness',whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness',darkColorLightness);
+}
+Bg1.addEventListener('click' , () =>{
+    // darkColorLightness = '95%';
+    // whiteColorLightness = '20%';
+    // lightColorLightness = '15%';
+
+    Bg1.classList.add('active');
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    window.location.reload();
+    // changeBG();
+
+});
+Bg2.addEventListener('click' , () =>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    Bg2.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+
+});
+Bg3.addEventListener('click' , () =>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    Bg3.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+
+});
